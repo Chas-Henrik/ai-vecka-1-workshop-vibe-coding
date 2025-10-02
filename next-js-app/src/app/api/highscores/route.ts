ace4c162e0b34300ea413ee220c19e56d8ccd64d
Henrik Suurik <henrik.suurik@chasacademy.se>
2025-10-02 13:44:38 +0200

    Add  src/app/api/highscores/route.ts to fetch the top 10 scores with usernames

---
+++ next-js-app/src/app/api/highscores/route.ts
@@ -0,0 +1,26 @@
+
+import { PrismaClient } from "@/generated/prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  try {
+    const highscores = await prisma.score.findMany({
+      take: 10,
+      orderBy: {
+        value: "desc",
+      },
+      include: {
+        user: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+    return NextResponse.json({ highscores });
+  } catch (error) {
+    return NextResponse.json({ error: "Something went wrong" }, { status: 500 });
+  }
+}

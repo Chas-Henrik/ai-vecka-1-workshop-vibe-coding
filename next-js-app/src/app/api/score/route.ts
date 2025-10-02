2b218f7204b551818f388b902937d426bc1c87f7
Henrik Suurik <henrik.suurik@chasacademy.se>
2025-10-02 13:43:08 +0200

    Add src/app/api/score/route.ts to save highscores to the database

---
+++ next-js-app/src/app/api/score/route.ts
@@ -0,0 +1,29 @@
+
+import { PrismaClient } from "@/generated/prisma/client";
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "@/lib/auth"
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function POST(req: NextRequest) {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const { score } = await req.json();
+
+  try {
+    const newScore = await prisma.score.create({
+      data: {
+        value: score,
+        userId: parseInt(session.user.id),
+      },
+    });
+    return NextResponse.json({ newScore }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ error: "Something went wrong" }, { status: 500 });
+  }
+}
